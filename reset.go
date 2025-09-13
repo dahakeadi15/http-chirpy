16ae39a57a568ea6ad7e9fd3fead7f770958b1b4
Aditya Dahake <dahakeadi15@gmail.com>
2025-09-13 21:03:47 +0530

    modify /reset to delete all users

--- reset.go
+++ reset.go
@@ -2,8 +2,11 @@ package main
 
 import "net/http"
 
-func (cfg *apiConfig) handlerResetMetrics(w http.ResponseWriter, req *http.Request) {
-	cfg.fileserverHits.Store(0)
+func (cfg *apiConfig) handlerResetMetrics(w http.ResponseWriter, r *http.Request) {
+	err := cfg.db.DeleteAllUsers(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "couldn't delete all users", err)
+		return
+	}
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0"))
 }
